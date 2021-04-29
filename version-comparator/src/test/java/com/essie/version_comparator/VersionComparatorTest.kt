749bec14e7720e2e9efce4723c846b99f3fdb33c
sung-eun <sungeunj0@naver.com>
2021-04-29 22:18:55 +0900

    Add test cases for custom comparator

--- version-comparator/src/test/java/com/essie/version_comparator/VersionComparatorTest.kt
+++ version-comparator/src/test/java/com/essie/version_comparator/VersionComparatorTest.kt
@@ -20,15 +20,64 @@ class VersionComparatorTest {
         assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0", "1.0.1.1"))
         assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0", "2.0.1"))
         assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0", "1.0.0_2"))
-        assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0.alpha", "1.0.0.beta"))
     }
 
     @Test
     fun greater() {
-//        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.1", "1.0.0"))
-//        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.1.1", "1.0.0"))
-//        assertEquals(GREATER_THAN, VersionComparator.compareTo("2.0.1", "1.0.0"))
-//        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0_2", "1.0.0"))
-        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0.beta", "1.0.0.alpha"))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.1", "1.0.0"))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.1.1", "1.0.0"))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("2.0.1", "1.0.0"))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0_2", "1.0.0"))
+    }
+
+    @Test
+    fun equals_CustomComparator() {
+        val comparator = createCustomComparator()
+        assertEquals(EQUALS, VersionComparator.compareTo("1.0.0.alpha", "1.0.0.alpha", comparator))
+        assertEquals(EQUALS, VersionComparator.compareTo("1.0.0.alpha_1", "1.0.0.alpha_1", comparator))
+    }
+
+    @Test
+    fun lessThan_CustomComparator() {
+        val comparator = createCustomComparator()
+        assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0.alpha", "1.0.0.beta", comparator))
+        assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0.alpha_1", "1.0.0.beta_1", comparator))
+        assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0.alpha_1", "1.0.0.alpha_2", comparator))
+        assertEquals(LESS_THAN, VersionComparator.compareTo("1.0.0.dev_1", "1.0.0.release_2", comparator))
+    }
+
+    @Test
+    fun greater_CustomComparator() {
+        val comparator = createCustomComparator()
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0.beta", "1.0.0.dev", comparator))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0.release", "1.0.0.beta", comparator))
+        assertEquals(GREATER_THAN, VersionComparator.compareTo("1.0.0.beta_4", "1.0.0.beta_1", comparator))
+    }
+
+    private fun createCustomComparator(): Comparator<String> {
+        return Comparator { o1, o2 ->
+            val ordinal1 = getOrdinalOfVersion(o1)
+            val ordinal2 = getOrdinalOfVersion(o2)
+
+            if (ordinal1 == ordinal2) {
+                return@Comparator o1.compareTo(o2)
+            }
+            return@Comparator ordinal1.compareTo(ordinal2)
+        }
+    }
+
+    private fun getOrdinalOfVersion(s: String): Int {
+        val lowerCase = s.toLowerCase()
+        return when {
+            lowerCase.startsWith("dev") -> TestBuildVariant.DEV.ordinal
+            lowerCase.startsWith("alpha") -> TestBuildVariant.ALPHA.ordinal
+            lowerCase.startsWith("beta") -> TestBuildVariant.BETA.ordinal
+            lowerCase.startsWith("release") -> return TestBuildVariant.RELEASE.ordinal
+            else -> -1
+        }
+    }
+
+    private enum class TestBuildVariant {
+        DEV, ALPHA, BETA, RELEASE
     }
 }
